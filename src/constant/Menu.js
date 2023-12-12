1e2e2a07c49741e3eacea0335e861b336ed2b9e4
ScaferuZ <endrafruz@gmail.com>
2023-12-12 17:00:07 +0700

    Making constants for the image source

---
+++ src/constant/Menu.js
@@ -0,0 +1,59 @@
+import Boxer1 from "../assets/pets/boxer_1.jpg";
+import Boxer2 from "../assets/pets/boxer_2.jpg";
+import Boxer3 from "../assets/pets/boxer_3.jpg";
+import Husky1 from "../assets/pets/husky_1.jpg";
+import Husky2 from "../assets/pets/husky_2.jpg";
+import Husky3 from "../assets/pets/husky_3.jpg";
+import Ragdoll1 from "../assets/pets/ragdoll cat_1.jpg";
+import Ragdoll2 from "../assets/pets/ragdoll cat_2.jpg";
+import Ragdoll3 from "../assets/pets/ragdoll cat_3.jpg";
+
+const Menu = [
+  {
+    id: 1,
+    image: Boxer1,
+    category: "Boxer",
+  },
+  {
+    id: 2,
+    image: Boxer2,
+    category: "Boxer",
+  },
+  {
+    id: 3,
+    image: Boxer3,
+    category: "Boxer",
+  },
+  {
+    id: 4,
+    image: Husky1,
+    category: "Husky",
+  },
+  {
+    id: 5,
+    image: Husky2,
+    category: "Husky",
+  },
+  {
+    id: 6,
+    image: Husky3,
+    category: "Husky",
+  },
+  {
+    id: 7,
+    image: Ragdoll1,
+    category: "Ragdoll",
+  },
+  {
+    id: 8,
+    image: Ragdoll2,
+    category: "Ragdoll",
+  },
+  {
+    id: 9,
+    image: Ragdoll3,
+    category: "Ragdoll",
+  },
+];
+
+export default Menu;
